a215adf90e9b8b7a49b02e8256b3949339c7540a
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-08-31 17:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+
+function getscreensize(){
+	let element = document.getElementById("sizeInfo");
+	let x = window.screen.width;
+	let y = window.screen.height;
+	element.innerText= "Width:" + " " + x + " " +"and"+ " "+ "Height:"+ y;
+}
